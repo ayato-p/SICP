4029a63c66999c4c54e14128a0c740ca87f43240
ayato0211 <ayato.skycrawlers@gmail.com>
2013-06-11 02:04:43 +0900

    ~q2.41

--- chapter_2/q2.41-p72.scm
+++ chapter_2/q2.41-p72.scm
@@ -1,3 +1,21 @@
 (add-load-path "/Users/ayato_p/Programming/SICP/lib" )
 (use sicp.util)
-aaa
+
+(define (unique-triple n)
+  (flatmap (lambda (i)
+	     (flatmap (lambda (j)
+			(map (lambda (k) (list i j k))
+			     (enumerate-interval 1 (- j 1))))
+		      (enumerate-interval 1 (- i 1))))
+	   (enumerate-interval 1 n)))
+
+;;think
+(let ((result (let ((n 10))
+		(map (lambda (i)
+		       (map (lambda (j) (list i j))
+			    (enumerate-interval 1 (- i 1))))
+		     (enumerate-interval 1 n)))))
+  (accumulate append '() result));;important!!
+
+(unique-triple 10)
+
